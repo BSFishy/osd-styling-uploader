1a31614eb1790290e6ecbd07f34ecf84e844549c
Matt Provost <provomat@amazon.com>
2023-09-14 18:25:41 +0000

    Fix docker image accumulation
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"
 yarn build
 yarn start:logs
 
-docker build --no-cache -t uploader .
+docker build --no-cache -t uploader:latest .
 docker run -t --rm --net=host uploader &>> logs/log.txt
+docker rmi -f uploader
